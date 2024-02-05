af0b00874414064a058b5a9f4d074f732d3c9f34
hoatran <tthoaptit@gmail.com>
2024-02-05 17:39:00 +0700

    Eclipse -tip

--- javaTester/eclipseTips/Topic_04_Access_Modifier.java
+++ javaTester/eclipseTips/Topic_04_Access_Modifier.java
@@ -2,4 +2,9 @@ package eclipseTips;
 
 public class Topic_04_Access_Modifier {
 
+	public static void main(String[] args) {
+		Topic_03_Getter_Setter topic_03 = new Topic_03_Getter_Setter();
+		topic_03.setCarName("Honda");
+		System.out.println(topic_03.getCarName());
+	}
 }
